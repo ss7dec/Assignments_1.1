fadeef29e891074001c078b19c9ee4f5ecc06a9b
ss7dec <ss7dec@gmail.com>
2017-06-12 00:02:56 +0530

    Add files via upload

---
+++ ACD_JAVAAB_SESSION_1_ASSIGNMENT_1.java
@@ -0,0 +1,10 @@
+//Write a program to print Hello World. 
+
+package question_1_1;
+
+
+public class ACD_JAVAAB_SESSION_1_ASSIGNMENT_1 {
+	public static void main(String []args) {
+	     	      System.out.println("Hello World");
+	   }
+}
